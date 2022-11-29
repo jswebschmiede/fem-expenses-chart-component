cb346685e43d2d1d631fe5b9dcf90676cc1fa262
jswebschmiede <joergschoeneburg@freenet.de>
2022-11-30 00:29:42 +0100

    set delay to default

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,7 +2,7 @@ export const fetchData = async () => {
   const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
   try {
-    await delay(2000);
+    await delay();
     const res = await fetch("data/data.json");
     const jsonData = await res.json();
     return jsonData;
